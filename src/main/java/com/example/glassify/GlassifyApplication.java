09f25ad0d27996ee33fd2e7d04602276fad1e173
doncheboj <doncheboj@telekom.mk>
2024-05-10 21:35:24 +0200

    Initial commit

---
+++ src/main/java/com/example/glassify/GlassifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.glassify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassifyApplication.class, args);
+    }
+
+}
